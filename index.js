e41e9b2383f17a45919bdb4f9c0ef84edbcd6673
Matt <43970143+VietTitans@users.noreply.github.com>
2024-03-13 23:36:56 +0100

    work in progress: Figure out how to lock layout in a 16*16 grid

--- index.js
+++ index.js
@@ -2,18 +2,25 @@ const body = document.body;
 const div = document.createElement("div");
 
 body.append(div);
-div.textContent = "Hello world";
+
+//Parent div
 div.style.outline = "1px";
 div.style.outlineColor = "blue";
 div.style.display = "flex";
-div.style.flexDirection = "row";
+div.style.flexWrap = "wrap";
 div.style.justifyContent = "center";
-div.style.alignItems = "center";
+div.style.height = "120px";
+div.style.width = "200px";
+
+//Child div
 createGrid();
 
 function createGrid(){
     for (let i = 0; i < 16 * 16; i++){
         let square = document.createElement("div");
+        square.style.width = "16px";
+        square.style.height = "16px";
+        square.style.outline = "1px dotted lightblue"; //Remove after completion
         div.append(square);
     }
 }
