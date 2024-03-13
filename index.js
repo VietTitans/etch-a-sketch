dca151fa01a4c590ed329242d13b3e9e1dbd743c
Matt <43970143+VietTitans@users.noreply.github.com>
2024-03-13 23:03:23 +0100

    Added divs for grid

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const body = document.body;
 const div = document.createElement("div");
+
+body.append(div);
+div.textContent = "Hello world";
+div.style.outline = "1px";
+div.style.outlineColor = "blue";
+div.style.display = "flex";
+div.style.flexDirection = "row";
+div.style.justifyContent = "center";
+div.style.alignItems = "center";
+createGrid();
+
+function createGrid(){
+    for (let i = 0; i < 16 * 16; i++){
+        let square = document.createElement("div");
+        div.append(square);
+    }
+}
