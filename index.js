fb37025a450db20b5524ae0befa41193bfc0dc7e
Matt <43970143+VietTitans@users.noreply.github.com>
2024-03-19 21:58:39 +0100

    Done with making 16x16 grid

--- index.js
+++ index.js
@@ -1,26 +1,25 @@
 const body = document.body;
 const div = document.createElement("div");
 
-body.append(div);
-
-//Parent div
-div.style.outline = "1px";
+//Container for squares
+div.style.outline = "10px";
 div.style.outlineColor = "blue";
 div.style.display = "flex";
 div.style.flexWrap = "wrap";
-div.style.justifyContent = "center";
-div.style.height = "120px";
-div.style.width = "200px";
+div.style.height = "16em";
+div.style.width = "16em";
 
-//Child div
+//squares
 createGrid();
 
 function createGrid(){
     for (let i = 0; i < 16 * 16; i++){
-        let square = document.createElement("div");
+        let square = document.createElement("divs");
         square.style.width = "16px";
         square.style.height = "16px";
-        square.style.outline = "1px dotted lightblue"; //Remove after completion
+        square.style.outline = "1px dotted lightblue";
         div.append(square);
     }
 }
+
+body.append(div);
