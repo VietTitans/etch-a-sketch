d34798915a0aca5a2799ef30c8cb2ccae0281674
Matt <43970143+VietTitans@users.noreply.github.com>
2024-04-12 20:07:13 +0200

    Implimented dynamic set grid to match userInput

--- index.js
+++ index.js
@@ -2,29 +2,24 @@
 const body = document.body;
 const div = document.createElement("div");
 
+let userInput = getUserInput();
+
+
 //Container for squares
 div.style.outline = "10px";
 div.style.outlineColor = "blue";
 div.style.display = "flex";
 div.style.flexWrap = "wrap";
-div.style.height = "16em";
-div.style.width = "16em";
+div.style.height = userInput + "em";
+div.style.width = userInput + "em";
 
 
-getUserInput();
-createGrid_();
-body.append(div);
+createGrid();
+setGridButton();
+body.append(div); //Grid div
 
 function getUserInput(){
-    //TODO Keep looping until valid int in parsed.
-    // let userInput = prompt("Please enter number (1-100) of squares per side");
-    // while (!isInLimit){
-    //     alert("Invalid input. Please enter a valid number");
-    //     let userInput = prompt("Please enter number (1-100) of squares per side");
-    // }
-    //     return userInput;
-    
-    let userInput; 
+    let userInput;
     while(isInLimit_){
         userInput = prompt("Please enter number (1-100) of squares per side ");
         if(!isNaN(userInput) && isInLimit_) {
@@ -40,20 +35,20 @@ function isInLimit_(){
     isInLimit_ = true;
     let minLimit = 1;
     let maxLimit = 100;
-    return userInput >= minLimit && userInput <= maxLimit;
+    return userInput >= minLimit || userInput <= maxLimit;
 }
 
-function createGrid_(){
-    userInput = getUserInput();
-    
+function createGrid(){
+    //  userInput = getUserInput();
+
     for (let i = 0; i < userInput * userInput; i++){
         let squares = document.createElement("div");
         squares.className = "square";
         squares.style.width = "16px";
         squares.style.height = "16px";
         squares.style.outline = "1px dotted lightblue";
-        squares.addEventListener("mouseout", removeHoverEffect);
-        squares.addEventListener("mouseover", applyHoverEffect);
+        squares.addEventListener("mouseout", _);
+        squares.addEventListener("mouseover", applyHoverEffect_);
         div.append(squares);
     }
 }
@@ -61,19 +56,19 @@ function createGrid_(){
 //Hover effect for grid
 let squares = document.querySelectorAll(".squares");
 squares.forEach(squares => {
-    squares.addEventListener("mouseover", applyHoverEffect);
-    squares.addEventListener("mouseout", removeHoverEffect);
+    squares.addEventListener("mouseover", applyHoverEffect_);
+    squares.addEventListener("mouseout", removeHoverEffect_);
 });
 
 function applyHoverEffect_(){
     this.style.backgroundColor = "lightblue"; 
 }
 
-function removeHoverEffect_(){
+function _(){
     this.style.backgroundColor = "";
 }
 
-function setGridButton_(){
+function setGridButton(){
     //Create button container
     const btnContainer = document.createElement("div");
     btnContainer.style.display = "flex";
@@ -81,7 +76,6 @@ function setGridButton_(){
     // btnContainer.style.justifyContent = "center";
     btnContainer.style.marginTop = "10px";
     btnContainer.style.marginBottom = "10px";
-
     document.body.append(btnContainer);
 
     //Create button element
